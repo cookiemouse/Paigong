68e0e9b17872477c08b7bee3f7eeabe83d5e36a0
cookiemouse <cookiemouse@163.com>
2017-09-07 13:18:13 +0800

    Bug 733

--- app/src/main/java/com/tianyigps/dispatch2/activity/OperateInstallActivity.java
+++ app/src/main/java/com/tianyigps/dispatch2/activity/OperateInstallActivity.java
@@ -170,7 +170,6 @@ public class OperateInstallActivity extends BaseActivity {
     protected void onPause() {
         super.onPause();
         myHandler.removeMessages(Data.MSG_1);
-        myHandler.removeMessages(Data.MSG_2);
     }
 
     @Override
